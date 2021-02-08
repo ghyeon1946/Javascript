aa6ac9c4414ec09107333a726c919783de56281a
ghyeon1946 <rudals1946@naver.com>
2021-02-08 20:43:29 +0900

    프로그래머스 수박수박수

--- watermelon.js
+++ watermelon.js
@@ -2,14 +2,13 @@ const solution = n => { return add(n)}
 
 function add(n) {
     let result = ''
-    
     if (n < 10000) 
     {
         for (let i = 0; i < n; i++) 
         {
             if(i % 2 === 0) { result +='수' }
             else { result += '박'}
-    	}   
+    	}
     }
     return result
 }
